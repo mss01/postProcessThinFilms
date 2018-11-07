34bec8e2b48003aba3f3ee52f8fdf72695c8bcdd
mss01 <maulikshah1009@gmail.com>
2018-11-07 16:35:03 +0100

    using older but more stable Matplotlib library

--- postProcessFilms.py
+++ postProcessFilms.py
@@ -1,7 +1,12 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
-%matplotlib inline
 import os
 import holoviews as hv
 hv.notebook_extension('matplotlib')
+
+dirList = os.listdir("dataFiles_few/")
+a = len(dirList)
 
